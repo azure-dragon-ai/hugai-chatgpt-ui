c7d6d880948f1871c051fbf449ae77cda53d22aa
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-09-03 19:55:58 +0800

    update

--- ui-admin/vue.config.js
+++ ui-admin/vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:7100`,
+        target: `http://152.136.174.88:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
